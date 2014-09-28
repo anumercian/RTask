51b4004423f485d081d6798f65703c7fe6d858b1
Anu Mercian <freebirdsanu@gmail.com>
2014-09-28 12:22:29 -0700

    Double figure point size

--- code/plot_inflammation.R
+++ code/plot_inflammation.R
@@ -14,5 +14,5 @@ data.in <- read.csv(options[1])
 # save png based on input from file name
 savename <- paste0(options[1],".png")
 png(filename=savename)
-plot(data.in$Day13, data.in$Day14, main = options[1], xlab = "Day 13", ylab = "Day 14")
+plot(data.in$Day13, data.in$Day14, main = options[1], xlab = "Day 13", ylab = "Day 14", cex = 2)
 dev.off()
